4f39f08e736ecb8c82250f44e6ddc2d6d5c8d7da
Miiles Horton <miileshorton@MacBook-Pro-de-CLARO.local>
2026-04-17 14:59:28 -0600

    Correct main preview image for Social Events

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -142,10 +142,10 @@ export const projectsData: Record<string, ProjectData> = {
         roleKey: "proj.social-events.role",
         descriptionKey: "proj.social-events.description",
         images: [
+            "imagenes preview/CFCFE098-B813-457F-AF6A-AACE6D8D0956.webp",
             "imagenes preview/A06BFBCB-522A-484D-BDC8-D7243858EAD2.webp",
             "portada.webp",
             "imagenes preview/69e3a06d-2a2b-471f-9e25-16bd90ecb40f.webp",
-            "imagenes preview/CFCFE098-B813-457F-AF6A-AACE6D8D0956.webp",
             "535708FF-AE0A-4563-A289-D22A06C42341.webp"
         ],
         participation: [
